b28a2d9d3961f94dd01e242c3051f0cf29933635
Kabir <kabirajibade@yahoo.com>
2023-02-11 17:35:58 +0100

    Done with backend and visit now to handle index and the rest

--- routes/pages.js
+++ routes/pages.js
@@ -15,15 +15,15 @@ router.get('/sites', (req, res) =>{
     res.sendFile(path.join(__dirname, '../', 'views', 'sites.html'))
 })
 
-router.get('/visit_us', (req, res)=>{
-    res.sendFile('../', 'views', 'visit_us.html')
+router.get('/visit', (req, res)=>{
+    res.sendFile(path.join(__dirname,'../', 'views', 'visit.html'))
 })
 
-router.post('/visitors', (req, res)=>{
-    const visitors=[req.body]
-    fs.appendFile('visitors.json', JSON.stringify(visitors), (err)=>{
+router.post('/visitor', (req, res)=>{
+    const visitors = [req.body]
+    fs.writeFile('visitors.json', JSON.stringify(visitors), (err)=>{
         if(err) throw "Unable to secure a ticket"
     })
-    res.statusCode-301;
+    res.statusCode=301;
     res.redirect('/');
 });module.exports=router;
